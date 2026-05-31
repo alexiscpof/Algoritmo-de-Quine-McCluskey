e9207c9fed8f630dc2b245459177332aef8d2008
Alexis Cristian <alexis.filho@dcomp.ufs.br>
2026-05-31 19:24:36 -0300

    Criação de um programa para testes da primeira fase do algoritmo

--- FuncaoLogica.java
+++ FuncaoLogica.java
@@ -1,12 +1,15 @@
 import java.util.*;
 
 public class FuncaoLogica {
+    // Uma função lógica é definida pelo número de variáveis e seu mintermos (saídas 1)
     private int numeroDeVariaveis;
     private Set<Integer> mintermos;
+    // Método construtor da classe
     public FuncaoLogica(int numeroDeVariaveis, Set<Integer> mintermos) {
         this.numeroDeVariaveis = numeroDeVariaveis;
         this.mintermos = mintermos;
     }
+    // Métodos getters
     public int getNumeroDeVariaveis() {
         return numeroDeVariaveis;
     }
--- Main.java
+++ Main.java
@@ -1,4 +1,24 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numeroDeVariaveis, numeroDeMintermos;
+        do {
+            System.out.printf("Digite o número de variáveis da função > ");
+            numeroDeVariaveis = scanner.nextInt();
+        } while (numeroDeVariaveis <= 0);
+        do {
+            System.out.printf("Digite o número de mintermos da função > ");
+            numeroDeMintermos = scanner.nextInt();
+        } while (numeroDeMintermos <= 0);
+        System.out.print("Digite os mintermos da função > ");
+        Set<Integer> mintermos = new HashSet<>();
+        for (int i = 0; i < numeroDeMintermos; i++) {
+            mintermos.add(scanner.nextInt());
+        }
+        FuncaoLogica funcao = new FuncaoLogica(numeroDeVariaveis, mintermos);
+        QuineMcCluskey algoritmo = new QuineMcCluskey(funcao);
+        System.out.print("Implicantes primos da função > ");
+        System.out.println(algoritmo.encontrarImplicantesPrimos().toString());
     }
 }
