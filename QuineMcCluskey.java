4d6a1bc40b97163f89e39554e3e5a22756ac78b6
Alexis Cristian <alexis.filho@dcomp.ufs.br>
2026-05-31 15:55:47 -0300

    Implementa os primeiros métodos do algoritmo

--- QuineMcCluskey.java
+++ QuineMcCluskey.java
@@ -1,3 +1,26 @@
+import java.util.*;
+
 public class QuineMcCluskey {
-    
+    private FuncaoLogica funcao;
+    public QuineMcCluskey(FuncaoLogica funcao) {
+        this.funcao = funcao;
+    }
+    private List<Termo> gerarTermosIniciais() {
+        List<Termo> termosIniciais = new ArrayList<>();
+        for (Integer mintermo: funcao.getMintermos()) {
+            String binario = Integer.toBinaryString(mintermo);
+            String binarioTratado = String.format("%" + funcao.getNumeroDeVariaveis() + "s", binario).replace(' ', '0');
+            termosIniciais.add(new Termo(binarioTratado));
+        }
+        return termosIniciais;
+    }
+    private Map<Integer, List<Termo>> agruparPorNumeroDeBits1(List<Termo> termos) {
+        Map<Integer, List<Termo>> termosAgrupados = new HashMap<>();
+        for (Termo termo : termos) {
+            int numeroDeBits1 = termo.getNumeroDeBits1();
+            termosAgrupados.putIfAbsent(numeroDeBits1, new ArrayList<>());
+            termosAgrupados.get(numeroDeBits1).add(termo);
+        }
+        return termosAgrupados;
+    }
 }
