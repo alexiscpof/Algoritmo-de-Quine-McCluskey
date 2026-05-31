90ff10b34e874b2becfac63fb7035edac50ddd6e
Alexis Cristian <alexis.filho@dcomp.ufs.br>
2026-05-31 03:19:18 -0300

    Implementação da classe Termo

---
+++ Termo.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Termo {
+    private String representacao;
+    private Set<Long> mintermosContemplados;
+    public Termo(String representacao) {
+        this.representacao = representacao;
+        mintermosContemplados = new HashSet<>();
+        determinarMintermosContemplados("", 0);
+    }
+    /* Método para determinar quais mintermos estão sendo contemplados pelo termo */ 
+    private void determinarMintermosContemplados(String termoAtual, int indice) {
+        /* Se o índice for maior que o número de caracteres da representação, significa que o mintermos já foi obtido
+        e já pode ser adicionado à lista de mintermos contemplados */
+        if (indice == representacao.length()) {
+            /* Conversão de uma String para número inteiro */
+            Long mintermoContemplado = Long.parseLong(termoAtual, 2);
+            mintermosContemplados.add(mintermoContemplado);
+            return;
+        }
+        char caractereAtual = representacao.charAt(indice);
+        /* Se o caractere atual for '-' chama a função recursivamente, escrevendo '0' e '1' e avançando o índice.
+        Caso contrário, escreve o caractere atual, chama a função recursivamente, escrevendo o caractere e avançando o índice */
+        if (caractereAtual == '-') {
+            determinarMintermosContemplados(termoAtual + '1', indice + 1);
+            determinarMintermosContemplados(termoAtual + '0', indice + 1);
+        } else {
+            determinarMintermosContemplados(termoAtual + caractereAtual, indice + 1);
+        }
+    }
+    public String getRepresentacao() {
+        return representacao;
+    }
+    public Set<Long> getMintermosContemplados() {
+        return mintermosContemplados;
+    }
+}
